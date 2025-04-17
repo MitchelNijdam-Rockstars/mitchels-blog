9e99f181e8872975aeac65cd18d0ccd48e7f1999
Mitchel Nijdam <mitchel.nijdam@teamrockstars.nl>
2025-04-17 23:14:09 +0200

    fix navigation 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import rehypeExternalLinks from 'rehype-external-links'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://MitchelNijdam-Rockstars.github.io',
-  base: 'mitchels-blog', // Needed for GitHub Pages since it's not at the root of the domain
+  base: '/mitchels-blog', // Needed for GitHub Pages since it's not at the root of the domain
   integrations: [mdx(), svelte()],
   markdown: {
     shikiConfig: {
